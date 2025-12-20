c20ba48b85db10d9f864d464109140026f11e372
ba2slk <0730bss@gmail.com>
2025-12-20 15:35:25 +0900

    chore: front Origin 추가

--- src/main/java/com/ceos22nd/voting_system/domain/auth/config/SecurityConfig.java
+++ src/main/java/com/ceos22nd/voting_system/domain/auth/config/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://next-vote-22nd-inky.vercel.app/"));
 
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
 
