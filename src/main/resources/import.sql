bbef05ac0bc60946b3eb9d49ea4ee423b66c9b0c
ba2slk <0730bss@gmail.com>
2025-12-20 20:08:08 +0900

    chore: 임시 팀 목록 추가

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO team (team_name) VALUES ('STORIX');
+INSERT INTO team (team_name) VALUES ('Modelly');
+INSERT INTO team (team_name) VALUES ('CatchUp');
+INSERT INTO team (team_name) VALUES ('Menual');
+INSERT INTO team (team_name) VALUES ('DiggIndie');
